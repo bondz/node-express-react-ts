de206443a072b1d9a66516d690ad4e947002a56c
Bond <lovedammy77@yahoo.com>
2020-07-26 14:31:19 +0100

    Add an example component to test Apollo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { gql, useQuery } from '@apollo/client';
+const query = gql`
+  {
+    sample {
+      hello
+    }
+  }
+`;
+function SampleComponent() {
+  const { data, loading, error } = useQuery(query);
+
+  if (loading) {
+    return <>Please wait</>;
+  }
+
+  if (error) {
+    console.error(error);
+
+    return (
+      <>
+        An Error occured: Check the console. It could be that the server is
+        offline, start the server and refresh.
+      </>
+    );
+  }
+
+  return <>Data Fetched: {data.sample.hello}</>;
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <SampleComponent />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
